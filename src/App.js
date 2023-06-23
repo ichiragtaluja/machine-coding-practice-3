773f1d0fd0327248467c97cdbf2c1cbd29cd68db
ichiragtaluja <Ichiragtaluja@gmail.com>
2023-06-23 21:56:43 +0530

    feat: mcr feat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { snacks } from "./data/snacksData";
 
 function App() {
+  const [searchedSnack, setSearchedSnack] = useState("");
+
+  const inputHandler = (e) => setSearchedSnack(e.target.value);
+
+  const [sortBy, setSortBy] = useState({ property: "", isAscending: false });
+
+  console.log("sortby", sortBy);
+
+  const snacksBySearch = searchedSnack
+    ? snacks.filter(
+        (snack) =>
+          snack?.product_name
+            ?.toLowerCase()
+            ?.includes(searchedSnack.toLowerCase()) ||
+          snack?.ingredients?.some((ingredient) =>
+            ingredient.toLowerCase()?.includes(searchedSnack.toLowerCase())
+          )
+      )
+    : snacks;
+
+  const snacksBySort = (sortBy) =>
+    sortBy
+      ? sortBy.property === "ingredient"
+        ? sortBy.isAscending
+          ? snacksBySearch.sort(
+              (a, b) =>
+                a[sortBy.property][0].charCodeAt() -
+                b[sortBy.property][0].charCodeAt()
+            )
+          : snacksBySearch.sort(
+              (a, b) =>
+                b[sortBy.property][0].charCodeAt() -
+                a[sortBy.property][0].charCodeAt()
+            )
+        : sortBy.isAscending
+        ? snacksBySearch.sort((a, b) => a[sortBy.property] - b[sortBy.property])
+        : snacksBySearch.sort((a, b) => b[sortBy.property] - a[sortBy.property])
+      : snacksBySearch;
+
+  // console.log(snacksBySort());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Snacks Table</h1>
+      <div className="table-section">
+        <input
+          onChange={inputHandler}
+          placeholder="Search with products or ingredients"
+        />
+        <div className="table">
+          <div className="table-header">
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "id"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "id" }
+                )
+              }
+              className="id"
+            >
+              ID
+            </div>
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "product_name"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "product_name" }
+                )
+              }
+              className="name"
+            >
+              Product Name
+            </div>
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "product_weight"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "product_weight" }
+                )
+              }
+              className="weight"
+            >
+              Product Weight
+            </div>
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "price"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "price" }
+                )
+              }
+              className="price"
+            >
+              Price INR
+            </div>
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "calories"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "calories" }
+                )
+              }
+              className="calories"
+            >
+              Calories
+            </div>
+            <div
+              onClick={() =>
+                setSortBy(
+                  sortBy.property === "ingredients"
+                    ? { ...sortBy, isAscending: !sortBy.isAscending }
+                    : { ...sortBy, property: "ingredients" }
+                )
+              }
+              className="ingredients"
+            >
+              Ingredients
+            </div>
+          </div>
+          {snacksBySort(sortBy)?.map((snack) => {
+            const {
+              id,
+              product_name,
+              product_weight,
+              price,
+              calories,
+              ingredients,
+            } = snack;
+
+            return (
+              <div className="table-row" key={id}>
+                <div className="id">{id}</div>
+                <div className="name">{product_name}</div>
+                <div className="weight">{product_weight}</div>
+                <div className="price">{price}</div>
+                <div className="calories">{calories}</div>
+                <div className="ingredients">
+                  {ingredients.map(
+                    (ingredient, index) =>
+                      `${ingredient} ${
+                        index === ingredients.length - 1 ? "" : ","
+                      }`
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
